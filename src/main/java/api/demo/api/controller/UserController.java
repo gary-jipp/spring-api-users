7f6a7d1c056a368451c9cfc80c3939ee6aff9d03
Gary J <github@accu.ca>
2025-03-12 13:23:44 -0700

    Ad Health & devtools

--- src/main/java/api/demo/api/controller/UserController.java
+++ src/main/java/api/demo/api/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,16 @@ public class UserController {
 
   @GetMapping("/user")
   public User getUser(@RequestParam Integer id) {
+    Optional<User> user = userService.getUser(id);
+    if (user.isPresent()) {
+      return (User) user.get();
+    }
+
+    return null;
+  }
+
+  @GetMapping("/user/{id}")
+  public User getUserPath(@PathVariable  Integer id) {
     Optional<User> user = userService.getUser(id);
     if (user.isPresent()) {
       return (User) user.get();
